fcb569c4ec9a65d56d2b785994fdc5a762c09895
Bas Bloemsaat <bas@bloemsaat.com>
2018-06-14 23:41:57 +0200

    cleanup

--- bin/get_latest_ergast_db.py
+++ bin/get_latest_ergast_db.py
@@ -6,10 +6,6 @@ import json
 import os
 import requests
 import shutil
-from subprocess import call
-
-from pprint import pprint
-from dumper import dump
 
 url = "http://ergast.com/downloads/f1db.sql.gz"
 status_file = os.path.join(os.path.dirname(__file__), "../data/ergast/state.json")
@@ -18,8 +14,6 @@ with open(status_file) as f:
     status = json.load(f)
 
 resp = requests.head(url)
-pprint(resp.headers)
-
 lastmod = resp.headers['Last-Modified']
 
 lastmod_date = parser.parse(lastmod)
@@ -29,7 +23,7 @@ size = int(resp.headers['Content-Length'])
 status_size = status['size']
 
 if (lastmod_date > status_lastmod_date or size != status_size):
-    print('UPDATE');
+    print('Getting new ergast db');
     try:
         resp = requests.get(url, stream=True)
         with open('/tmp/f1db.sql.gz', 'wb') as out_file:
@@ -46,7 +40,8 @@ if (lastmod_date > status_lastmod_date or size != status_size):
         status['last-modified'] = lastmod
     except (RuntimeError, TypeError, NameError):
         pass
-
+else:
+    print('Already up to date, not refreshing')
 
 with open(status_file, 'w') as outfile:
     json.dump(status, outfile)
