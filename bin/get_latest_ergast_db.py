17b0b8f0f765e271228e665def8c88dad44c14f6
Bas Bloemsaat <bas@bloemsaat.com>
2018-06-14 23:37:30 +0200

    phase one, get db to sqlite file

---
+++ bin/get_latest_ergast_db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+from dateutil import parser
+import json
+import os
+import requests
+import shutil
+from subprocess import call
+
+from pprint import pprint
+from dumper import dump
+
+url = "http://ergast.com/downloads/f1db.sql.gz"
+status_file = os.path.join(os.path.dirname(__file__), "../data/ergast/state.json")
+
+with open(status_file) as f:
+    status = json.load(f)
+
+resp = requests.head(url)
+pprint(resp.headers)
+
+lastmod = resp.headers['Last-Modified']
+
+lastmod_date = parser.parse(lastmod)
+status_lastmod_date = parser.parse(status['last-modified'])
+
+size = int(resp.headers['Content-Length'])
+status_size = status['size']
+
+if (lastmod_date > status_lastmod_date or size != status_size):
+    print('UPDATE');
+    try:
+        resp = requests.get(url, stream=True)
+        with open('/tmp/f1db.sql.gz', 'wb') as out_file:
+            shutil.copyfileobj(resp.raw, out_file)
+
+        os.system('gunzip /tmp/f1db.sql.gz -f')
+
+        mysql2sqlite = os.path.join(os.path.dirname(__file__), "mysql2sqlite")
+        ergastsqlite = os.path.join(os.path.dirname(__file__), "../data/ergast/ergast_sqlite3.db")
+        os.system('rm ' + ergastsqlite)
+        os.system(mysql2sqlite + ' /tmp/f1db.sql | sqlite3 ' + ergastsqlite)
+
+        status['size'] = size
+        status['last-modified'] = lastmod
+    except (RuntimeError, TypeError, NameError):
+        pass
+
+
+with open(status_file, 'w') as outfile:
+    json.dump(status, outfile)
