13443c2657f5f45501ea3d5e206d7153526c705c
Bas Bloemsaat <bas@bloemsaat.com>
2018-07-02 08:20:38 +0200

    changes

--- bin/create_ergast_json_data.py
+++ bin/create_ergast_json_data.py
@@ -9,25 +9,29 @@ from pprint import pprint
 from dumper import dump
 
 ergastsqlite = os.path.join(os.path.dirname(
-    __file__), "../data/ergast/ergast_sqlite3.db")
+    __file__), "../../racing-data-munging-prv/ergast/ergast_sqlite3.db")
 uri = 'file:' + ergastsqlite + '?mode=ro'
 
 db = sqlite3.connect(uri, uri=True)
 db.isolation_level = None
 
 
+def save_to_json(data, json_file):
+    path = os.path.join(os.path.dirname(__file__),
+                        "../data/ergast/", json_file)
+    print('saving ' + path)
+    with open(path, 'w') as outfile:
+        json.dump(data, outfile)
+
+
 def exec_query(query, args=[], json_file=None):
     c = db.cursor()
-    c.execute(query)
+    c.execute(query, args)
     names = [d[0] for d in c.description]
     rows = [dict(zip(names, row)) for row in c.fetchall()]
 
     if json_file:
-        path = os.path.join(os.path.dirname(__file__),
-                            "../data/ergast/", json_file)
-        with open(path, 'w') as outfile:
-            json.dump(rows, outfile)
-
+        save_to_json(rows, json_file)
     return rows
 
 
@@ -58,3 +62,68 @@ for season in seasons:
         parents=True,
         exist_ok=True
     )
+
+drivers = exec_query(
+    query='''
+        SELECT 
+            d.driverId, d.driverRef, d.forename, d.surname, d.dob as date_of_birth, d.nationality, d.url
+        FROM 
+            drivers d
+    '''
+)
+
+for driver in drivers:
+    driver_id = driver['driverId']
+    del driver['driverId']
+
+    driver_years_active = exec_query(
+        '''
+            SELECT 
+                ra.year
+                , count(DISTINCT re.raceId) as races
+                , SUM(CASE WHEN position=1 THEN 1 ELSE 0 END) as wins
+                , SUM(CASE WHEN position IN (1,2,3) THEN 1 ELSE 0 END) podiums
+            FROM 
+                races ra
+                LEFT JOIN results re ON re.raceId = ra.raceId
+            WHERE re.driverId = ?
+            GROUP BY ra.year
+        ''',
+        [driver_id]
+    )
+
+    driver['years_active'] = driver_years_active
+
+save_to_json(drivers, 'drivers.json')
+
+constructors = exec_query(
+    query='''
+        SELECT 
+            c.constructorId, c.constructorRef, c.name, c.nationality, c.url
+        FROM constructors c
+    '''
+)
+
+for constructor in constructors:
+    constructor_id = constructor['constructorId']
+    del constructor['constructorId']
+
+    constructor_years_active = exec_query(
+        '''
+            SELECT 
+                ra.year
+                , count(DISTINCT re.raceId) as races
+                , SUM(CASE WHEN position=1 THEN 1 ELSE 0 END) as wins
+                , SUM(CASE WHEN position IN (1,2,3) THEN 1 ELSE 0 END) podiums
+            FROM 
+                races ra
+                LEFT JOIN results re ON re.raceId = ra.raceId
+            WHERE re.constructorId = ?
+            GROUP BY ra.year
+        ''',
+        [constructor_id]
+    )
+
+    constructor['years_active'] = constructor_years_active
+
+save_to_json(constructors, 'constructors.json')
