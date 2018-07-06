68470baed4a0e227852608f1d9568940782b6d67
Bas Bloemsaat <bas@bloemsaat.com>
2018-07-06 09:42:18 +0200

    constructors minimized

--- explore/js/rdm.js
+++ explore/js/rdm.js
@@ -1,1 +1,31 @@
 var rdm = {};
+
+(function() {
+    this.reduce_sum = function(variable) {
+        return function(a, b) {
+
+            xx = { variable: a[variable] + b[variable] };
+            console.log(xx);
+
+            var x = {};
+            x[variable] = a[variable] + b[variable];
+            return x
+        };
+    }
+    this.reduce_min = function(variable) {
+        return function(a, b) {
+            var x = {};
+            x[variable] = a[variable] < b[variable] ? a[variable] : b[variable];
+            return x
+        };
+    }
+
+    this.reduce_max = function(variable) {
+        return function(a, b) {
+            var x = {};
+            x[variable] = a[variable] > b[variable] ? a[variable] : b[variable];
+            return x
+        };
+    }
+
+}).apply(rdm);
