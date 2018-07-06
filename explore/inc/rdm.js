ad8697d96f19ef8f1af20d2dae88a6938360a81a
Bas Bloemsaat <bas@bloemsaat.com>
2018-07-06 11:20:00 +0200

    refactor + cleanup

--- explore/inc/rdm.js
+++ explore/inc/rdm.js
@@ -3,15 +3,12 @@ var rdm = {};
 (function() {
     this.reduce_sum = function(variable) {
         return function(a, b) {
-            var i = 0;
-            // xx = { i++: a[variable] + b[variable] };  \\`
-            // console.log(xx);
-
             var x = {};
             x[variable] = a[variable] + b[variable];
             return x
         };
     }
+
     this.reduce_min = function(variable) {
         return function(a, b) {
             var x = {};
@@ -27,5 +24,24 @@ var rdm = {};
             return x
         };
     }
+
+    this.sortby = function(data, sort_term_function, sort_direction) {
+        data.sort(function(a, b) {
+            key_a = sort_term_function(a);
+            key_b = sort_term_function(b);
+            if (key_a > key_b) return (sort_direction == 'asc' ? 1 : -1);
+            if (key_a < key_b) return (sort_direction == 'asc' ? -1 : 1);
+            return 0;
+        })
+        return data;
+    }
+
+    this.current_sort = {
+        title: "none",
+        order: "unsorted",
+    }
+
+    this.loaded_data = {};
+
 
 }).apply(rdm);
