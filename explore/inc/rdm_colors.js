b066fec7f26dfc10bc4a4fd38087a0f8bc0278e8
Bas Bloemsaat <bas@bloemsaat.com>
2018-08-10 16:16:58 +0200

    wip

--- explore/inc/rdm_colors.js
+++ explore/inc/rdm_colors.js
@@ -1,23 +1,3 @@
-var team_colors = {
-    "2018": {
-        "ferrari": "#DC0000",
-        "force_india": "#F596C8",
-        "haas": "#5A5A5A",
-        "mclaren": "#FF8700",
-        "mercedes": "#00D2BE",
-        "red_bull": "#00327D",
-        "renault": "#FFF500",
-        "sauber": "#9B0000"
-        "toro_rosso": "#0032FF",
-        "williams": "#EEEEEE",
-    },
-    "fallback": {
-        "ferrari": "#FF2800",
-        "unknown": "#444444",
-    },
-};
-
-
 (function() {
     this.team_color = function(ref, year) {
         if (team_colors[year] && team_colors[year][ref]) {
@@ -29,3 +9,40 @@ var team_colors = {
         return team_colors["fallback"]["unknown"];
     }
 }).apply(rdm);
+
+var team_colors = {
+    "2018": {
+        "ferrari": "#DC0000",
+        "force_india": "#F596C8",
+        "haas": "#5A5A5A",
+        "mclaren": "#FF8700",
+        "mercedes": "#00D2BE",
+        "red_bull": "#00327D",
+        "renault": "#FFF500",
+        "sauber": "#9B0000",
+        "toro_rosso": "#0032FF",
+        "williams": "#EEEEEE",
+    },
+    "2017": {
+        "ferrari": "#C30000",
+        "force_india": "#FF80C7",
+        "haas": "#6C0000",
+        "mclaren": "#FF7B08",
+        "mercedes": "#00CFBA",
+        "red_bull": "#00007D",
+        "renault": "#FFD800",
+        "sauber": "#006EFF",
+        "toro_rosso": "#0000FF",
+        "williams": "#EEEEEE",
+    },
+    "fallback": {
+        "ferrari": "#FF2800",
+        "lotus": "#FFB800",
+        "marussia": "#6E0000",
+        "red_bull": "#4F1AAB",
+        "sauber": "#0063FF",
+        "toro_rosso": "#0005C1",
+        "unknown": "#444444",
+    },
+};
+
