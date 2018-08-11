3276027f9ac54eddbeed9dd7e319dd3376da2c9e
Bas Bloemsaat <bas@bloemsaat.com>
2018-08-11 22:17:10 +0000

    finish vs classification

--- explore/inc/rdm.js
+++ explore/inc/rdm.js
@@ -41,6 +41,29 @@ var rdm = {};
         return data;
     }
 
+    this.svg_add_stripes = function(svg) {
+        var defs = svg.append('defs');
+        defs.append('pattern')
+            .attr('id', 'pattern-stripe')
+            .attr('width', 4)
+            .attr('height', 4)
+            .attr('patternUnits', 'userSpaceOnUse')
+            .attr('patternTransform', 'rotate(45)')
+            .append('rect')
+            .attr('width', 2)
+            .attr('height', 4)
+            .attr('transform', 'translate(0,0)')
+            .attr('fill', 'white');
+        defs.append('mask')
+            .attr('id', 'mask-stripe')
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', '100%')
+            .attr('height', '100%')
+            .attr('fill', 'url(#pattern-stripe)');
+    }
+
     this.current_sort = {
         title: "none",
         order: "unsorted",
